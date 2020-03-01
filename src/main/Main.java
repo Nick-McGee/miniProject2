37c769bb04c37f2c8706424d73b8d84357f7f1b4
Nicholas McGee <nicholas.r.mcgee@gmail.com>
2020-02-29 16:41:32 -0800

    Switched to using Runnable

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,9 @@
 package main;
 
-import java.util.Random;
 import java.util.Scanner;
 
-public class Main extends Thread{
+public class Main implements Runnable{
+
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
@@ -17,10 +17,12 @@ public class Main extends Thread{
         for(int i = 0; i < slaves; i++) {
             int wait = (int)(Math.random() * (10-5 + 1)) + 5;  // create random wait time
 
+            // output producer text
             System.out.println(String.format("Producer: Produced request ID %d, length %d seconds at time %s", i+1, maxTime, java.time.LocalTime.now()));
             System.out.println(String.format("Producer: Sleeping for %d seconds", wait));
 
-            (new Main()).start();  // creating the slave threads
+            // this add them to the queue, not start them
+            (new Thread(new Main())).start(); // create slave thread and run it
 
             // master wait time
             try {
@@ -33,15 +35,15 @@ public class Main extends Thread{
 
     // running the thread
     public void run() {
-        System.out.println("Thread sleeping...");
+        System.out.println("Thread is busy.");
 
         // thread wait time
         try {
-            Thread.sleep(3000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Thread waking up.");
+        System.out.println("Thread is idle.");
     }
 }
